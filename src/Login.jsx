2bdfe3e1e33d9f65a0788d3ec4546fca9885cdaa
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-14 09:50:50 +0100

    up

--- src/Login.jsx
+++ src/Login.jsx
@@ -25,6 +25,11 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+        const response = await axios.post(LOGIN_URL, JSON.stringify({user, pwd}), {
+            headers: {'Content-Type': 'application/json'}, 
+            withCredentials: true
+        })
+
       setSuccess(true);
       user("");
       pwd("");
