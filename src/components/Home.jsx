4cb46cca0252a3f1b72e90f1ea884d30d01d0e27
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-14 14:11:22 +0100

    up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom"
 
 
 const Home = () => {
   return (
     <section>
-      
+      <h1>Home</h1>
+      <br />
+      <p>You are logged in</p>
+      <br />
+        <Link to='./Editor'>Go to the Editor Page</Link>
+      <br />
+        <Link to='./Admin'>Go to the Admin Page</Link>
+      <br />
+        <Link to='./Lounge'>Go to the Lounge Page</Link>
+      <br />
+        <Link to='./LinkPage'>Go to the Link Page</Link>
     </section>
   )
 }
