adbe4d9d2752b52184559996b8e2fe75db393738
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-14 16:12:32 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,13 @@ import Editor from "./components/Editor";
 import Admin from "./components/Admin";
 import Lounge from "./components/Lounge";
 import Missing from "./components/Missing";
+import RequireAuth from "./components/RequireAuth";
+
+const ROLES = {
+  'User': 2001,
+  'Editor':1984,
+  'Admin':5158
+}
 
 const App = () => {
   return (
@@ -19,10 +26,18 @@ const App = () => {
         <Route path="/LinkPage" element={<LinkPage />} />
         <Route path="/unauthorized" element={<Unauthorized />} />
         {/* private route */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]}/>}>
         <Route path='/' element={<Home/>} />
+        </Route>
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]}/>}>
         <Route path='/editor' element={<Editor/>} />
+        </Route>
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]}/>}>
         <Route path='/admin' element={<Admin/>} />
+        </Route>
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]}/>}>
         <Route path='/lounge' element={<Lounge/>} />
+        </Route>
         {/* catch all err */}
         <Route path="*" element={<Missing />}/>
       </Routes>
