3fe32396f886d83992cb5b51394a382f5edae056
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-14 15:15:15 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import Login from './components/Login'
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./components/Login";
+import LinkPage from "./components/LinkPage";
+import Unauthorized from "./components/Unauthorized";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <main className='App'>
-      <Login />
+    <main className="App">
+      <Routes>
+        <Route path="/" element={<Layout/>}/>
+        {/* public route */}
+        <Route index element={<Login />} />
+        <Route path="/LinkPage" element={<LinkPage />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+        {/* private route */}
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
