0c089f7ae1cbd7d5bda63609a8ffbdeeb5cb8455
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-14 15:19:57 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Login from "./components/Login";
 import LinkPage from "./components/LinkPage";
 import Unauthorized from "./components/Unauthorized";
 import Home from "./components/Home";
+import Editor from "./components/Editor";
+import Admin from "./components/Admin";
+import Lounge from "./components/Lounge";
+import Missing from "./components/Missing";
 
 const App = () => {
   return (
@@ -16,6 +20,11 @@ const App = () => {
         <Route path="/unauthorized" element={<Unauthorized />} />
         {/* private route */}
         <Route path='/' element={<Home/>} />
+        <Route path='/editor' element={<Editor/>} />
+        <Route path='/admin' element={<Admin/>} />
+        <Route path='/lounge' element={<Lounge/>} />
+        {/* catch all err */}
+        <Route path="*" element={<Missing />}/>
       </Routes>
     </main>
   );
