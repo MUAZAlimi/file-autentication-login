8f80d16bedfc9a6d7d433e32641ca5cde0eb24a6
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-14 09:47:40 +0100

    up

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,95 +1,94 @@
 import { useEffect, useState, useRef, useContext } from "react";
 import AuthContext from "./context/AuthProvider";
+import axios from "./api/axios";
 
 const Login = () => {
-    const {setAuth} = useContext(AuthContext)
+  const LOGIN_URL = "/auth";
+  const { setAuth } = useContext(AuthContext);
 
-    const userRef = useRef()
-    const errRef = useRef()
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [errMsg, setErrMsg] = useState("")
-    const [success, setSuccess] = useState(false)
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    useEffect(() => {
-      userRef.current.focus()
-    }, [])
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    useEffect(() => {
-        setErrMsg("")
-    }, [username, password])
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if(!username || !password) {
-            setErrMsg('Invalid Entry');
-            return;
-          }
-          console.log(username, password)
-          setSuccess(true)
-          username("")
-          password("")
-      }
-    
-    
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setSuccess(true);
+      user("");
+      pwd("");
+    } catch (err) {}
+    // if(!user || !password) {
+    //     setErrMsg('Invalid Entry');
+    //     return;
+    //   }
+  };
+
   return (
     <>
-        {success ? ( 
+      {success ? (
         <section>
-        <h1>Login successfully</h1>
-        <p>
-          <a href="./Login">Home</a>
-        </p>
-      </section>
-
-    ) :(
+          <h1>Login successfully</h1>
+          <p>
+            <a href="./Login">Home</a>
+          </p>
+        </section>
+      ) : (
         <section>
-            <p ref={errRef} className={ errMsg ? "errMsg": "offscreen"} aria-live="assertive">
-                {errMsg}
-            </p>    
-            <h1>sign in</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Username:
-                    <input 
-                    type="text"
-                    id="username"
-                    ref={userRef}
-                    autoComplete="off"
-                    onChange={(e) => setUsername(e.target.value)}
-                    value={username}
-                    required
-                    />
-                </label>
-                <label>
-                    Password:
-                    <input 
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                    />
-                </label>
-                <button
-                
-                >
-                    Sign in
-                </button>
-            </form>
-            <p>
-                 Need an Acount ? <br />
-                 <span className="line">
-                    <a href="./Login">
-                        Sign in
-                    </a>
-                 </span>
-            </p>
-        </section>     
-    )} 
+          <p
+            ref={errRef}
+            className={errMsg ? "errMsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>sign in</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Username:
+              <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+              />
+            </label>
+            <label>
+              Password:
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
+            </label>
+            <button>Sign in</button>
+          </form>
+          <p>
+            Need an Acount ? <br />
+            <span className="line">
+              <a href="./Login">Sign in</a>
+            </span>
+          </p>
+        </section>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
