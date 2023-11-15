95985480d6e2d36f5cfe1ab4fbbc93ea1ab7b105
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-15 09:23:07 +0100

    up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,36 @@
-import { Link } from "react-router-dom"
-
+import { useNavigate, Link } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
 
 const Home = () => {
-  return (
-    <section>
-      <h1>Home</h1>
-      <br />
-      <p>You are logged in</p>
-      <br />
-        <Link to='./Editor'>Go to the Editor Page</Link>
-      <br />
-        <Link to='./Admin'>Go to the Admin Page</Link>
-      <br />
-        <Link to='./Lounge'>Go to the Lounge Page</Link>
-      <br />
-        <Link to='./LinkPage'>Go to the Link Page</Link>
+    const { setAuth } = useContext(AuthContext);
+    const navigate = useNavigate();
 
-        <div className="flexGrow">
-            <button>Sign Out</button>
-        </div>
-    </section>
-  )
+    const logout = async () => {
+        // if used in more components, this should be in context 
+        // axios to /logout endpoint 
+        setAuth({});
+        navigate('/linkpage');
+    }
+
+    return (
+        <section>
+            <h1>Home</h1>
+            <br />
+            <p>You are logged in!</p>
+            <br />
+            <Link to="/editor">Go to the Editor page</Link>
+            <br />
+            <Link to="/admin">Go to the Admin page</Link>
+            <br />
+            <Link to="/lounge">Go to the Lounge</Link>
+            <br />
+            <Link to="/linkpage">Go to the link page</Link>
+            <div className="flexGrow">
+                <button onClick={logout}>Sign Out</button>
+            </div>
+        </section>
+    )
 }
 
 export default Home
