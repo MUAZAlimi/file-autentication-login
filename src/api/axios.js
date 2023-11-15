70283f5e887bf0c68b7df5a5d3894466fa9f5a8b
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-15 16:34:59 +0100

    up

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:4900'
+const BASE_URL = 'http://localhost:3000'
+
 export default axios.create({
-    baseURL : BASE_URL
-})
+    baseURL: BASE_URL 
+});
 export const axiosPrivate = axios.create({
-    baseURL : BASE_URL,
+    baseURL: BASE_URL,
     headers: {'Content-Type': 'application/json'},
     withCredentials: true
 })
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,48 +1,52 @@
-import {  useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
+
 
 const Users = () => {
     const [users, setUsers] = useState();
     const axiosPrivate = useAxiosPrivate();
-    // const refresh = useRefreshToken()
+    
+   // const refresh = useRefreshToken()
 
     useEffect(() => {
-        let isMounted = true
-        const controller = new AbortController()
+        let isMounted = true;
+        const controller = new AbortController();
 
         const getUsers = async () => {
             try {
                 const response = await axiosPrivate.get('/users', {
                     signal: controller.signal
-                })
+                });
                 console.log(response.data);
-                isMounted && setUsers(response.data)
-            } catch (error) {
-                console.log(error);
+                isMounted && setUsers(response.data);
+            } catch (err) {
+                console.error(err);
+                
             }
         }
-        getUsers()
+
+        getUsers();
+
         return () => {
             isMounted = false;
             controller.abort();
         }
-
     }, [])
 
     return (
         <article>
-            <h2>User List</h2>
+            <h2>Users List</h2>
             {users?.length
                 ? (
                     <ul>
-                        {users.map((user, i) => <li key={i} >{user?.
-                            username}</li>)}
+                        {users.map((user, i) => <li key={i}>{user?.username}</li>)}
                     </ul>
-                ) : <p>No user to display</p>
+                ) : <p>No users to display</p>
             }
-          
+          {/* <button onClick={() => refresh()}>Refresh</button>
+          <br /> */}
         </article>
-    )
-}
+    );
+};
 
-export default Users
+export default Users;
