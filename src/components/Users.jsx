90cbdaebf0d8b2e2ea8e27ed7af1f908ea557925
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-15 10:51:16 +0100

    up

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react"
+import axios from "../api/axios"
 
 const Users = () => {
   const [users, setUsers] = useState()
-  
+
   return (
-    <div>
-      
-    </div>
+    <article>
+      <h2>Users List </h2>
+      {users?.length
+          ? (
+              <ul>
+                   {users.map((user, i) => <li key={i}>{user?.username}</li>)}
+              </ul>
+          ) : <p>No user to display</p>
+      }
+    </article>
   )
 }
 
