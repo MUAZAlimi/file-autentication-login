d7967aa6c87261487127b6d72c7e598f422ffda7
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-15 09:21:34 +0100

    up

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,18 +1,21 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
+import { useLocation, Navigate, Outlet } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
 
 // eslint-disable-next-line react/prop-types
-const RequireAuth = ({ allowedRoles }) => {
-  const { auth } = useAuth();
-  const location = useLocation();
-  // eslint-disable-next-line react/prop-types
-  return auth?.roles?.find((role) => allowedRoles?.includes(role)) ? (
+const RequireAuth = ({allowedRoles}) => {
+    const { auth } = useAuth()
+    const location = useLocation()
+  return (
+    // eslint-disable-next-line react/prop-types
+    auth?.roles?.find(role => allowedRoles.includes(role)) ?
     <Outlet />
-  ) : auth?.user ? (
-    <Navigate to="/unauthorized" state={{ from: location }} replace />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
-};
+    :
+    auth?.user?
+    <Navigate to="/unauthorized" state={{from: location}} replace/>
+    :
+    <Navigate to="/login" state={{from: location}} replace/>
+    )
+}
 
-export default RequireAuth;
+export default RequireAuth
