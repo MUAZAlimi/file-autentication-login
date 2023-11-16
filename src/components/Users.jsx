7c08d10ccb54665e4d16e34303cfd9063c493635
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-16 09:42:42 +0100

    up

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 const Users = () => {
    const [users, setUsers] = useState([]);
    const axiosPrivate = useAxiosPrivate()
+   const navigate = useNavigate();
+   const location = useLocation();
 
    useEffect(() => {
       let isMounted = true;
@@ -19,6 +22,7 @@ const Users = () => {
             isMounted && setUsers(response.data);
          } catch (err) {
             console.log(err);
+            navigate('/login', {state: {from: location}, replace: true});
          } 
       }; 
       getUsers();
