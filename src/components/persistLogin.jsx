ef8f3594c4bb658abc4f93061aaf259083070135
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-16 15:00:54 +0100

    UP

--- src/components/persistLogin.jsx
+++ src/components/persistLogin.jsx
@@ -3,52 +3,48 @@ import { useEffect, useState } from "react";
 import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
 
-
 const persistLogin = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [isLoading, setIsLoading] = useState(true)
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const refresh = useRefreshToken()
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [isLoading, setIsLoading] = useState(true);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const refresh = useRefreshToken();
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { auth } = useAuth()
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { auth, persist } = useAuth();
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-                // eslint-disable-next-line no-unused-vars
-                // let isMounted = true
-        const verifyRefreshToken = async () => {
-            try {
-                  await refresh();
-            } catch (error) {
-                console.error(error);
-            } finally {
-                 setIsLoading(false)
-            }
-        }
-        !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    // eslint-disable-next-line no-unused-vars
+    // let isMounted = true
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
 
-        //  return () => isMounted = false;
-    }, [])
+    //  return () => isMounted = false;
+  }, []);
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-            console.log(`isLoading : ${isLoading}`)
-            console.log(`aT: ${JSON.stringify(auth?.accessToken)}`)
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    console.log(`isLoading : ${isLoading}`);
+    console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isLoading])
-    
-    return (
-        <>
-            {isLoading ?
-                 <p>Loading...</p>
-                 :
-                 <Outlet/>
-            }
-        </>
-    )
-    
+  }, [isLoading]);
 
-}
+  return (
+    <>{!persist
+         ? <Outlet /> 
+         : isLoading 
+                ? <p>Loading...</p>
+                : 
+                <Outlet />}</>
+  );
+};
 
-export default persistLogin
+export default persistLogin;
