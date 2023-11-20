549d688bb82186b9c5720301dccabb8e37660ed8
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-20 11:07:36 +0100

    up

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -4,8 +4,12 @@ const useToggle = (key, initValue) => {
    const [value, setValue] = useLocalStorage(key, initValue)
 
    const toggle = (value) => {
-        
+        setValue(prev => {
+            return typeof value === "boolean"  ? value : !prev
+        })
    }
+
+   return [value, toggle]
 }
 
 export default useToggle
